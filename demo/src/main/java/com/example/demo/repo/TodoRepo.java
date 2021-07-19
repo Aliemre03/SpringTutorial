b6b29a60abdf9c402bc88baaf8d97ba67b72ca1d
Ali Emre Deneri <ali.emre.deneri@gmail.com>
2021-07-19 11:42:58 +0300

    inital commit

--- demo/src/main/java/com/example/demo/repo/TodoRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repo;
-
-import com.example.demo.model.TodoItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepo extends JpaRepository<TodoItem,Long> {
-}
